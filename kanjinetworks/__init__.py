842d9dfc1fce2f9b187e22fb6bcc4b4311b26e26
Arnaud Coomans <arnaud.coomans@gmail.com>
2017-08-19 00:14:31 -0400

    Added support for Japanese app version 4

--- kanjinetworks/__init__.py
+++ kanjinetworks/__init__.py
@@ -1,4 +1,4 @@
 from kanji import Kanji
 from extract.extractor import get_text
 from extract.parser import KanjiNetworksParser
-from export.japaneseapp import JapaneseAppExporter
+from export.japaneseapp import JapaneseApp3Exporter, JapaneseApp4Exporter
--- scripts/kn_to_ja.py
+++ scripts/kn_to_ja.py
@@ -4,7 +4,7 @@ import argparse
 
 from kanjinetworks import get_text
 from kanjinetworks import KanjiNetworksParser
-from kanjinetworks import JapaneseAppExporter
+from kanjinetworks import JapaneseApp3Exporter, JapaneseApp4Exporter
 
 # def statistics(text):
 #     number_of_empty_lines = 0
@@ -17,6 +17,7 @@ if __name__ == "__main__":
 
     argparser = argparse.ArgumentParser(description='Replace notes in a backup file from the Japanese iOS app with Kanji Networks etymologies.')
     argparser.add_argument('filename', type=str, help='Japanese app backup file')
+    argparser.add_argument('-v', '--version', type=int, help='Japanese app version (3,4)', default=4)
     args = argparser.parse_args()
 
     text = get_text()
@@ -27,4 +28,9 @@ if __name__ == "__main__":
     #     print unicode(kanji)
     print "imported %s kanjis" % len(kanjis)
 
-    JapaneseAppExporter().replace_notes_in_file(args.filename, kanjis)
+    if args.version is 3:
+        print "exporting to version 3 ..."
+        JapaneseApp3Exporter().replace_notes_in_file(args.filename, kanjis)
+    if args.version is 4:
+        print "exporting to version 4 ..."
+        JapaneseApp4Exporter().replace_notes_in_file(args.filename, kanjis)
